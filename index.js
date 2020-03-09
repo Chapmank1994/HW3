3c38a9f459bc20b0aa875b8d1c1b077353e7702b
Chapmank1994 <60373275+Chapmank1994@users.noreply.github.com>
2020-03-08 17:39:50 -0700

    Getting Closer

--- index.js
+++ index.js
@@ -1,2 +1,26 @@
 // Prompt for user input
-var userInp = window.prompt("Enter start Number:")
+// var then name / Will save into "name"
+// REMEMBER that the Users Input will be saved into the window.prompt
+
+var startInst = window.prompt("Enter start Number")
+var endInst = window.prompt("Enter End number:");
+var stepInst = window.prompt("Enter Step Number:");
+
+// How to turn the Users input number(strings) into actual numerical values.
+// These are the actual UserInputs
+startnum = parseInt(startInst);
+endnum = parseInt(endInst);
+stepnum = parseInt(stepInst);
+
+array = ()
+sum = (startnum + endnum + stepnum)
+binary = ()
+
+
+
+// prints out selected numbers, "<br/> used to break lines of output strings"
+// Plus step number funtion
+// Plus-Step funtion results turned into binary
+document.write("The generated array is " + array + "<br/>");
+document.write("The Sum is" + sum +"<br/>");
+document.write("The binary of absolut element values are " binary + "<br/>");
